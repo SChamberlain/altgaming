43b4e82e0acabe07fca6f66a67de8519db202ffe
ian mulvany <ian@mulvany.net>
2012-11-03 15:18:24 -0700

    added bins to intervals function to see how may signals are in each bin

--- bin-to-events.py
+++ bin-to-events.py
@@ -3,6 +3,7 @@ import csv
 time_range = "months" # days
 path = "/Users/ian/Dropbox/alm12-sf-hackathon/"
 test_file = path + "journal.pone.0042446.csv"
+virtual_time = 10000 # an abstract time space to hoist events into, at an instance level.
 
 def read_data(file): # get the data from a local csv file
 	data_points = []
@@ -24,12 +25,36 @@ def un_cumulate_bins(cumulate_bins):
 		instance_bins.append(delta)
 	return instance_bins
 
-def bins_to_instances(instance_bins):
-	instances = []
-	for bin in instance_bins:
-		print bin
-		
+def calculate_time_per_bin(virtual_time, bins):
+	return virtual_time / bins 
+
+def bins_to_intervals(instance_bins, virtual_time):
+	time_per_bin = virtual_time / len(instance_bins)
+	intervals = []
+	current_time = 0
+	print_time = 0
+	signals = []
+	for bin in instance_bins: 
+		if bin > time_per_bin:
+			# there is not enough virtual time, we have a signal for every interval
+			interval = 1
+			print "OMFG, you are throwing away data, you crazy fool"
+		else:
+			if int(bin) == 0:
+				# there is no signal, the interval is the entire allocation of virtual time
+				interval = time_per_bin
+			else:
+				# we have a real signal that we jump along.
+				interval = time_per_bin/int(bin)
+		intervals.append(interval)
+	return intervals
+
+def intervals_to_signals(intervals, time_per_bin):
+	print intervals, time_per_bin
+	signals = []
+
 points = read_data(test_file)
 instance_bins = un_cumulate_bins(points)
-print instance_bins
-instances = bins_to_instances(instance_bins)
+intervals = bins_to_intervals(instance_bins, virtual_time)
+time_per_bin = calculate_time_per_bin(virtual_time, len(instance_bins))
+signals = intervals_to_signals(intervals, time_per_bin) 
