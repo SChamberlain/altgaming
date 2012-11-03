e1f96a6a01ebb22e1066d43252c6aea0b51716ee
Scott Chamberlain <myrmecocystus@gmail.com>
2012-11-03 11:14:57 -0700

    started coding

---
+++ altgaming.r
@@ -0,0 +1,7 @@
+# gaming/signal detection
+library(rplos); library(ggplot2); library(plyr)
+setwd("/Users/ScottMac/Dropbox/alm12-sf-hackathon")
+dir()
+dat <- read.csv("alm_report_2012-09-12.csv")
+
+
