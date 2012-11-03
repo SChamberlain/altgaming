a6f315819e3a2f5242d3f04b899270d0c9eafec4
dartar <dario.taraborelli@gmail.com>
2012-11-03 15:48:53 -0700

    Corrected doc

--- parse.sh
+++ parse.sh
@@ -1,9 +1,10 @@
-# quick and dirty parser to slice the dump and generate CSV files with the following naming scheme: ./series/[source]/[doi].csv, e.g.
+# usage: cat sample.tsv | ./parse.sh
+#
+# quick and dirty parser to slice the dump and store time series data into CSV files with the following naming scheme: s_[source]_[doi].csv, e.g.
 #
 # 	./series/wikipedia/journal.pone.0043486.csv
-# 
-# each file holds comma separated lines: date,count, e.g.
-# 	2012-10-12,0
-# 	2012-10-13,1
+#
+# [source] is the ALM source, e.g. 'wikipedia', 'twitter', etc.
+# [doi] is the prefix-less DOI of the article
 #
 sed '1d;s/"//g' | gawk -F, '!($4$1$2 in q){q[$4$1$2]; a=substr($4,9,30); f="s_"$1"_"a".csv"; print $2","$3 > f}'
