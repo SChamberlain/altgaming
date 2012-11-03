cab2521fef72005bdda3773f20cc27c0ca56730f
Scott Chamberlain <myrmecocystus@gmail.com>
2012-11-03 11:23:43 -0700

    code using to get data

--- altgaming.r
+++ altgaming.r
@@ -2,6 +2,7 @@
 library(rplos); library(ggplot2); library(plyr)
 setwd("/Users/ScottMac/Dropbox/alm12-sf-hackathon")
 dir()
-dat <- read.csv("alm_report_2012-09-12.csv")
+dois <- read.csv("alm_report_2012-09-12.csv")[,1]
 
-
+out <- lapply(dois[1:3000], function(x) almplosallviews(x, info='detail')$history)
+length(out[[2]])
